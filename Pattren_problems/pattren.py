654ddd95a04da7cb8a511e4e041567a923645513
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-10 15:21:25 +0530

    update

---
+++ Pattren_problems/pattren.py
@@ -0,0 +1,107 @@
+import sys
+
+def square_pattren(n):
+    for i in range(n-1):
+        for j in range(n-1):
+            print("*",end=" ")
+        print()
+
+# s=square_pattren(5)
+# print(s)
+
+def pattren2(n):
+    for i in range(n+1):
+        for j in range(i):
+            print("*",end=" ")
+        print()
+    return " "
+
+# s2 = pattren2(5)
+# print(s2)
+
+
+def pattren3(n):
+    for i in range(n+1):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        print()
+    return " "
+
+# s3 = pattren3(5)
+# print(s3)
+
+
+def pattren4(n):
+    for i in range(n+1):
+        for j in range(0,i):
+            print(i,end=" ")
+        print()
+    return " "
+
+# s3 = pattren4(5)
+# print(s3)
+
+def pattren5(n):
+    for i in range(n):
+        for j in range(n,i,-1):
+            print("*",end=" ")
+        print()
+    return " "
+
+# s3 = pattren5(5)
+# print(s3)
+
+def pattren6(n):
+    for i in range(n,0,-1):
+        for j in range(n+1,i,-1):
+            print(i,end=" ")
+        print()
+    return ""
+
+# s3 = pattren6(5)
+# print(s3)
+
+
+def pattren7(n):
+    for i in range(n):
+        for j in range(n,i,-1):
+            print(n-j+1,end=" ")
+        print()
+    return " "
+
+# s4 = pattren7(5)
+# print(s4)
+
+
+def pattren8(n):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ",end="")
+        for j in range(2*i+1):
+            print("*",end=" ")
+        for j in range(n-i-1):
+            print(" ",end="")
+        print()
+
+    return  " "
+
+# s4 = pattren8(5)
+# print(s4)
+
+def pattren9(n):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ",end="")
+
+        for j in range(n*2-(2*i+1)):
+            print("*",end=" ")
+
+        for j in range(n-i-1):
+            print(" ",end="")
+
+        print()
+
+    return " "
+
+s5 = pattren9(5)
+print(s5)

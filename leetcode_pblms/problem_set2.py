81b0121f4c3abdf139ad82466a9f28031f011e9a
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-13 12:29:10 +0530

    update

--- leetcode_pblms/problem_set2.py
+++ leetcode_pblms/problem_set2.py
@@ -11,3 +11,12 @@ def contains_duplicate(num):
 
 nums = [1,2,3,4,1]
 print(contains_duplicate(nums))
+
+
+def first_duplicate(s):
+    seen = set()
+    for i in s:
+        if i in seen:
+             return i
+        seen.add(i)
+    return None

4d64cc360538d70c442e0932438f527815a37982
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-16 23:22:30 +0530

    update

--- leetcode_pblms/leetcode.py
+++ leetcode_pblms/leetcode.py
@@ -130,3 +130,28 @@ class Solution(object):
         nums.append(word1[i:])
         nums.append(word2[i:])
         return "".join(nums)
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {"I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000}
+        total = 0
+        for i in range(len(s)):
+            if i+1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+        return total
+
+        
+        

e8b090cc0762cfcc3d6308572901c9a12acb0834
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-16 21:45:32 +0530

    update

--- leetcode_pblms/leetcode.py
+++ leetcode_pblms/leetcode.py
@@ -111,3 +111,22 @@ class Solution(object):
             elif abs(close) == abs(num) and num > close:
                 close = num
         return close
+    
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        nums = []
+        i = 0
+
+        while i < len(word1) and i < len(word2):
+            nums.append(word1[i])
+            nums.append(word2[i])
+            i+=1
+        nums.append(word1[i:])
+        nums.append(word2[i:])
+        return "".join(nums)

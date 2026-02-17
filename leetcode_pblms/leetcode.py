7ea3fd58f0047a4c5489817478c8bcbe428be5b6
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-17 12:13:13 +0530

    update

--- leetcode_pblms/leetcode.py
+++ leetcode_pblms/leetcode.py
@@ -167,3 +167,23 @@ def get_top_k_elements(nums,k):
     for i in range(k):
         result.append(get_sorted_list[i][0])
     return result
+
+# get longestConsecutive number
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_set = set(nums)
+        longest = 0
+
+        for num in nums:
+            if (num-1) not in num_set:
+                start=num
+                length = 1
+                while (start+1) in num_set:
+                    length +=1
+                    start +=1
+                longest = max(length,longest)
+        return longest

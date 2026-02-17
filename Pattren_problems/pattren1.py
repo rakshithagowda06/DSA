b42df7f8b6a1e1f25e5c12f3a5dde928773ea968
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-17 10:37:03 +0530

    update

--- Pattren_problems/pattren1.py
+++ Pattren_problems/pattren1.py
@@ -1,11 +1,32 @@
 # Dictionary and set pattrens
 
 """ Count ocuurence of elements"""
-# Frequency counter
-
+# Pattern 1: Frequency Counter
 
 def freq_counter(nums):
     freq = {}
     for num in nums:
         freq[num] = freq.get(num,0) + 1
     return freq
+
+# Pattern 2: Seen/Visited Tracker
+
+"""Track what you've already processed"""
+items = []
+seen = set()
+for item in items:
+    if item in seen:
+        continue
+    seen.add(item)
+
+# Pattern 3: Two Sum Pattern
+
+"""Use complement lookup"""
+lookup = {}
+nums = []
+target = 0
+for i in range(len(nums)):
+    compliment = target - nums[i]
+    if compliment in lookup:
+        print([lookup[i],i])
+    lookup[nums[i]] = i

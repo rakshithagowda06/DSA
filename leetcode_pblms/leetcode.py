c7a21a6bbdc3bfefb4feb382fa0c27f4a92dfccd
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-20 00:37:30 +0530

    update

--- leetcode_pblms/leetcode.py
+++ leetcode_pblms/leetcode.py
@@ -221,6 +221,23 @@ class Solution(object):
 
 
          
+# Check palindrome or not
+
+def check_palindrome(s):
+    cleaned = ""
+    for char in s:
+        if char.isalnum():
+            cleaned += char.lower()
+    left = 0
+    right = len(cleaned)-1
+
+    while left < right:
+        if cleaned[left] != cleaned[right]:
+            return False
+        left +=1
+        right -=1
+
+    return True
 
 
 

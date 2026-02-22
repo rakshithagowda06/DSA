50af03bca96aa1f046a9cf9236619ff1b618acdd
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-22 23:22:02 +0530

    update

--- leetcode_pblms/leetcode.py
+++ leetcode_pblms/leetcode.py
@@ -281,6 +281,7 @@ class Solution(object):
                 right -= 1
         return max_water
     
+ # check its a valid parantesis
 
 class Solution(object):
     def isValid(self, s):
@@ -302,4 +303,52 @@ class Solution(object):
         return len(stack)==0
 
 
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
         
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            curr_min = self.min_stack[-1]
+            self.min_stack.append(min(val,curr_min))
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.min_stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_stack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

ed99045455364369827177ba29897613e232d548
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-22 21:28:17 +0530

    update

--- leetcode_pblms/leetcode.py
+++ leetcode_pblms/leetcode.py
@@ -259,5 +259,25 @@ class Solution(object):
                 right -= 1
                 
 
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(height) - 1
+        max_water =0
 
+        while left<right:
+            width = right - left
+            height1 = min(height[left],height[right])
+            area = height1 * width
+            max_water = max(max_water,area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_water
         

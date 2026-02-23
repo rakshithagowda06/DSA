ce7b87fff97cdc99e558767fd9b8571afc3b5bb3
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-23 13:15:20 +0530

    update

--- leetcode_pblms/leetcode.py
+++ leetcode_pblms/leetcode.py
@@ -367,3 +367,18 @@ def daily_tempreature(temps):
     return stack
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverseList(head):
+    prev = None
+    curr = head
+    while curr:
+        next_node = curr.next
+        curr.next = prev
+
+        prev = curr
+        curr = next_node
+    return prev

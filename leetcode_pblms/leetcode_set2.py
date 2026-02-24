ca1ff474b5c5d1e91b000ccf160a91c7bb705d0a
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-24 11:14:54 +0530

    update

--- leetcode_pblms/leetcode_set2.py
+++ leetcode_pblms/leetcode_set2.py
@@ -1,6 +1,6 @@
 """ Tree problems"""
 
-
+# maxdepth of a tree 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -22,3 +22,29 @@ root.right.right = TreeNode(7)
 root.right.right.left = TreeNode(8)
 root.right.right.right = TreeNode(10)
 print(maxDepth(root))
+
+# Invert a tree
+
+
+def invertTree(root):
+    if not root:
+        return None
+    
+    # Swap using tuple unpacking (more Pythonic!)
+    root.left, root.right = root.right, root.left
+    
+    # Recursively invert
+    invertTree(root.left)
+    invertTree(root.right)
+    
+    return root
+
+
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(7)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(9)

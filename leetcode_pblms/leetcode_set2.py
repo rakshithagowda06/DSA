984edcd37a52428a8a65adfa9c64d33009c1dcdd
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-25 13:21:23 +0530

    update

--- leetcode_pblms/leetcode_set2.py
+++ leetcode_pblms/leetcode_set2.py
@@ -88,3 +88,24 @@ class Solution(object):
                     bfs(r,c)
         return islands            
         
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums)==1:
+            return nums[0]
+        
+        n = len(nums)
+        dp = [0] * n
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+
+        for i in range(2,n):
+            dp[i] = max(nums[i]+dp[i-2],dp[i-1])
+        return dp[n-1]

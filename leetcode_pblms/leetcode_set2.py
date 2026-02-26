831e74d096c8fac475863183953c142c3dc02c29
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-26 23:54:32 +0530

    update

--- leetcode_pblms/leetcode_set2.py
+++ leetcode_pblms/leetcode_set2.py
@@ -122,3 +122,16 @@ def firstBadversion(n):
         else:
             left = mid + 1
     return left
+
+
+from typing import List
+
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        elements = set()
+        for number in arr:
+            if 2 * number in elements or (number % 2 == 0 and number // 2 in elements):
+                return True
+            elements.add(number)
+        return False

ca95aa4439d6926e3e297b915d6833b96ddb3e7e
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-02-27 10:52:23 +0530

    update

--- leetcode_pblms/tree/set1.py
+++ leetcode_pblms/tree/set1.py
@@ -15,3 +15,30 @@ def lowestcommonAncestor(root,p,q):
             node = node.right               # move to right subtree
         else:                               # p and q are split OR one of them equals current node
             return node                     # current node is the LCA, return it
+
+
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def helper_func(node,min_val,max_val):
+            if not node:
+                return True
+
+            if node.val <= min_val or node.val >= max_val:
+                return False
+
+            return (helper_func(node.left,min_val,node.val) and
+                   helper_func(node.right,node.val,max_val))
+        return helper_func(root,float('-inf'),float('inf'))

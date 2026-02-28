74a5b12cd4f40aad6912ac9cec704ffb2eddeda5
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-03-01 00:15:44 +0530

    update

--- leetcode_pblms/top100/set1.py
+++ leetcode_pblms/top100/set1.py
@@ -51,3 +51,19 @@ def max_sum(nums):
     return max_sum
 
         
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        n = len(t)
+        i = 0
+
+        for j in range(n):
+            if i < len(s) and s[i] == t[j]:
+                i+=1
+        return i == len(s)
+        

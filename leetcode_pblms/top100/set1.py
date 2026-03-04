7a7f1c4bf216bfa264411ad47761ae00bc9a2e16
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-03-04 21:20:38 +0530

    update

--- leetcode_pblms/top100/set1.py
+++ leetcode_pblms/top100/set1.py
@@ -135,3 +135,29 @@ class Solution(object):
             result.append(str(start) + "->" + str(nums[-1]))
         
         return result
+
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if not intervals:
+            return []
+        
+        # Step 1: Sort intervals by start time
+        intervals.sort(key=lambda x: x[0])
+        
+        merged = [intervals[0]]
+        
+        for current in intervals[1:]:
+            prev = merged[-1]
+            
+            # If overlapping
+            if current[0] <= prev[1]:
+                prev[1] = max(prev[1], current[1])
+            else:
+                merged.append(current)
+        
+        return merged

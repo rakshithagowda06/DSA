da652356bcd7fa8be1b8f23c46272295b8df7002
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-03-05 23:23:09 +0530

    update

--- leetcode_pblms/top100/set2.py
+++ leetcode_pblms/top100/set2.py
@@ -32,3 +32,23 @@ class Solution(object):
                 left += 1
 
         return res
+
+
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        
+        n = len(matrix)
+
+        # Step 1: Transpose
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Step 2: Reverse each row
+        for row in matrix:
+            row.reverse()

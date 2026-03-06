b31855b1d3a5a8141da5df3335cc83cbb6228dfc
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-03-06 19:43:08 +0530

    update

--- leetcode_pblms/top100/set2.py
+++ leetcode_pblms/top100/set2.py
@@ -52,3 +52,19 @@ class Solution(object):
         # Step 2: Reverse each row
         for row in matrix:
             row.reverse()
+
+
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        count = 0
+
+        for i in stones:
+            if i in jewels:
+                count += 1
+        return count
+        

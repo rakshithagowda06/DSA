787b5107fd61c037ec7b632d84c5e6a4dac966a5
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-03-10 14:59:24 +0530

    update

--- leetcode_pblms/top100/set2.py
+++ leetcode_pblms/top100/set2.py
@@ -259,4 +259,20 @@ class Solution(object):
             result.append(num*num)
         result.sort()
         return result
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            s[left],s[right] = s[right],s[left]
+            left += 1
+            right -= 1
+            
+
         

173304cee7f981a845c0678f0a34d56486356c98
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-03-10 14:29:04 +0530

    update

--- leetcode_pblms/top100/set2.py
+++ leetcode_pblms/top100/set2.py
@@ -229,4 +229,20 @@ class Solution(object):
 
                 
             
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        counts = {}
+        for num in nums:
+            if num not in counts:
+                counts[num] = 1
+            else:
+                counts[num] += 1
+        n = len(nums)
+        for num in counts:
+            if counts[num] > (n/2):
+                return num
         

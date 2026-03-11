175732cd13848d6c4e72dc28568560c83bf65cd0
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-03-11 23:11:29 +0530

    update

--- leetcode_pblms/top100/set2.py
+++ leetcode_pblms/top100/set2.py
@@ -312,5 +312,23 @@ class Solution(object):
                     
 
 
+class Solution(object):
+    def calPoints(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        stack = []
 
+        for op in operations:
+            if op!='C' and op!='D' and op!='+':
+                stack.append(int(op))
+            elif op=='C':
+                stack.pop()
+            elif op=='D':
+                stack.append(stack[-1]*2)
+            elif op=='+':
+                stack.append(stack[-1] + stack[-2])
+        return sum(stack)
+        
         

60267cbdae79d23fa1754b4c753738dfd949d254
rakshithagowda06 <rakshithapandu06@gmail.com>
2026-03-12 22:07:29 +0530

    update

--- leetcode_pblms/top100/set2.py
+++ leetcode_pblms/top100/set2.py
@@ -376,4 +376,25 @@ class Solution(object):
                 answers[prev_index] = i - prev_index
             stack.append(i)
         return answers
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
         
